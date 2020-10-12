4c90cfa50f1312a6d47844c134fdfc23702ca5c4
Lixin <lixxue@ethz.ch>
2020-10-12 22:01:35 +0200

    fix a typo in test.py

--- test.py
+++ test.py
@@ -14,8 +14,8 @@ def test(num_pcs, max_num_grids):
 
   for i in range(num_pcs):
     num_grids = np.random.randint(low=0, high=max_num_grids)
-    prefix_sum_cpu(grid_cnt, num_grids, grid_off)
-    prefix_sum_cuda(grid_cnt_cuda, num_grids, grid_off_cuda)
+    prefix_sum_cpu(grid_cnt[i], num_grids, grid_off[i])
+    prefix_sum_cuda(grid_cnt_cuda[i], num_grids, grid_off_cuda[i])
     print(grid_off[i, :20])
     print(grid_off_cuda[i, :20])
 
