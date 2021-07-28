947ff26390582364681ebca6141250fafc7edc0f
xju <xju@cern.ch>
2021-07-28 13:28:29 -0700

    add options to allow nvcc arguments

--- setup.py
+++ setup.py
@@ -6,12 +6,19 @@ class BuildExtension(torch.utils.cpp_extension.BuildExtension):
     def __init__(self, *args, **kwargs):
         super().__init__(use_ninja=False, *args, **kwargs)
 
+nvcc_args = []
+nvcc_flags_env = os.getenv("NVCC_FLAGS", "")
+if nvcc_flags_env != "":
+    nvcc_args.extend(nvcc_flags_env.split(" "))
+
+extra_compile_args["nvcc"] = nvcc_args
+
 setup(
   name="prefix_sum",
   author="Matt Dean, Lixin Xue",
   description="Parallel Prefix Sum on CUDA with Pytorch API",
   ext_modules=[
-    CUDAExtension('prefix_sum', ['prefix_sum.cu'])
+    CUDAExtension('prefix_sum', ['prefix_sum.cu'], extra_compile_args=extra_compile_args)
   ],
   cmdclass={"build_ext": BuildExtension},
 )
